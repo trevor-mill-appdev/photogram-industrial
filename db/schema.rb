7779aecc4b79d2d46c8b6425c8e821b81944dad3
trevor-mill <trevor.million@chicagobooth.edu>
2022-04-18 15:19:45 +0000

    fixed db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_174630) do
+ActiveRecord::Schema.define(version: 2022_04_18_145231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "photo_id", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["photo_id"], name: "index_comments_on_photo_id"
+  end
+
+  create_table "follow_requests", force: :cascade do |t|
+    t.bigint "recipient_id", null: false
+    t.bigint "sender_id", null: false
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipient_id"], name: "index_follow_requests_on_recipient_id"
+    t.index ["sender_id"], name: "index_follow_requests_on_sender_id"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string "image"
@@ -44,5 +64,9 @@ ActiveRecord::Schema.define(version: 2021_04_16_174630) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "photos"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "follow_requests", "users", column: "recipient_id"
+  add_foreign_key "follow_requests", "users", column: "sender_id"
   add_foreign_key "photos", "users", column: "owner_id"
 end
