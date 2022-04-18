6280e8db5fe9a4aaae0f271e0ff36d696e303309
trevor-mill <trevor.million@chicagobooth.edu>
2022-04-18 22:54:35 +0000

    Set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
+  resources :users, only: :show
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
