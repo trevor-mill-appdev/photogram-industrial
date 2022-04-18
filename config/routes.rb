f3c07e3c236ca511defcf5a31dfa5c32c707b0fb
trevor-mill <trevor.million@chicagobooth.edu>
2022-04-18 23:37:34 +0000

    Added commenting

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
   root "photos#index"
 
+  
+
   devise_for :users
 
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
-  resources :users, only: :show
+
+
+
+
+
+  
+  get "/:username" => "users#show"
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
