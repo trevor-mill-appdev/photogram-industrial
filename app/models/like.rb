4e6ad4f04b671ce5282b4be9fc61e9b501ddd150
trevor-mill <trevor.million@chicagobooth.edu>
2022-04-18 16:31:46 +0000

    Counter caches added

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,15 +3,25 @@
 # Table name: likes
 #
 #  id         :bigint           not null, primary key
-#  fan_id     :bigint           not null
-#  photo_id   :bigint           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  fan_id     :bigint           not null
+#  photo_id   :bigint           not null
+#
+# Indexes
+#
+#  index_likes_on_fan_id    (fan_id)
+#  index_likes_on_photo_id  (photo_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (fan_id => users.id)
+#  fk_rails_...  (photo_id => photos.id)
 #
 class Like < ApplicationRecord
   validates(:fan_id, presence: true)
   validates(:photo_id, presence: true)
 
   belongs_to :fan, class_name: "User", counter_cache: :likes_count
-  belongs_to :photo, counter_cache: :likes_count
+  belongs_to :photo, counter_cache: :likes_count, counter_cache: true
 end
