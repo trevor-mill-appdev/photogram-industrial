e9239e801efbd3ca2918f6e9ee8aa6c38ec6059d
trevor-mill <trevor.million@chicagobooth.edu>
2022-04-18 16:37:47 +0000

    Added direct associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,8 @@ class User < ApplicationRecord
   
   has_many :comments, foreign_key: "author_id"
 
-  has_many :follow_requests
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
 
   has_many :likes, foreign_key: "fan_id"
 
